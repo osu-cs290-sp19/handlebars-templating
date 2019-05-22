d1e4556fa5d5dcdb505d1017825861447ef37a26
Rob Hess <robwhess@gmail.com>
2019-05-22 10:13:03 -0700

    Add code from 5/22 lecture.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var path = require('path');
 var express = require('express');
-var app = express();
+var exphbs = require('express-handlebars');
 
+var app = express();
 var port = 3001;
+
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
@@ -17,16 +21,30 @@ var availablePeople = [
   'finn',
   'r2d2'
 ];
-
 app.get('/people/:person', function (req, res, next) {
-  var person = req.params.person.toLowerCase();
-  if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
-  } else {
-    next();
-  }
+
+  res.render('photoPage', {
+    name: "Kitten",
+    photos: [
+      {
+        url: "http://placekitten.com/480/480?image=8",
+        caption: "Kitty"
+      },
+      {
+        url: "http://placekitten.com/480/480?image=3",
+        caption: "Another kitty"
+      }
+    ]
+  });
+
+  // var person = req.params.person.toLowerCase();
+  // if (availablePeople.indexOf(person) >= 0) {
+  //   res.status(200).sendFile(
+  //     __dirname + '/public/people/' + person + '.html'
+  //   );
+  // } else {
+  //   next();
+  // }
 });
 
 app.get("*", function (req, res, next) {
